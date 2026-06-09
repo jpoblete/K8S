57af448530498f0d75893c7f89e25bfe4948d118
José Martínez Poblete <j.martinez.poblete@gmail.com>
2026-06-09 16:31:53 -0600

    Refactor createTable.sh to improve readability

--- hive/base/createTable.sh
+++ hive/base/createTable.sh
@@ -44,130 +44,126 @@ echo "iterations  : number of partitions to be created"
 [  -z "${ROW_PART}"    ] && echo "INFO : rows is undefined, defaulting to rows=10"              && ROW_PART=10
 [  -z "${ITERATIONS}"  ] && echo "INFO : iterations is undefined, defaulting to iteration=10"   && ITERATIONS=10
 
-# Performance helper: Generates an alphanumeric string entirely in-memory
-genRandomStr() {
-    local len=$1
-    local s=""
-    local chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    while (( ${#s} < len )); do
-        s+="${chars:RANDOM%62:1}"
-    done
-    echo "$s"
-}
 createTbl(){
-STATEMENT="CREATE "
-[ "${TBL_TYPE}" == "ext" ] && STATEMENT="${STATEMENT} EXTERNAL"
-STATEMENT="${STATEMENT} TABLE IF NOT EXISTS ${TBL_NAME} ( c1 int,"
-((TBL_COLS--))
-for i in $(seq 2 ${TBL_COLS}); do
-STATEMENT="${STATEMENT} c${i} string, "
-done
-((TBL_COLS++))
-STATEMENT="${STATEMENT} c${TBL_COLS} string )"
-if [ "${PRT_COUNT}" -gt 0 ]; then
-STATEMENT="${STATEMENT} PARTITIONED BY ("
-((PRT_COUNT--))
-for i in $(seq 1 ${PRT_COUNT}); do
-STATEMENT="${STATEMENT} p${i} int, "
-done
-((PRT_COUNT++))
-STATEMENT="${STATEMENT} p${PRT_COUNT} int )"
-else
-STATEMENT="${STATEMENT}"
-fi
-STATEMENT="${STATEMENT} CLUSTERED BY (c1) SORTED BY (c1 ASC) INTO 128 BUCKETS;"
-echo ${STATEMENT}
+   STATEMENT="CREATE "
+   [ "${TBL_TYPE}" == "ext" ] && STATEMENT="${STATEMENT} EXTERNAL"
+   STATEMENT="${STATEMENT} TABLE IF NOT EXISTS ${TBL_NAME} ( c1 int,"
+   ((TBL_COLS--))
+   for i in $(seq 2 ${TBL_COLS}); do
+       STATEMENT="${STATEMENT} c${i} string, "
+   done
+   ((TBL_COLS++))
+   STATEMENT="${STATEMENT} c${TBL_COLS} string )"
+   if [ "${PRT_COUNT}" -gt 0 ]; then
+      STATEMENT="${STATEMENT} PARTITIONED BY ("
+      ((PRT_COUNT--))
+      for i in $(seq 1 ${PRT_COUNT}); do
+          STATEMENT="${STATEMENT} p${i} int, "
+      done
+      ((PRT_COUNT++))
+      STATEMENT="${STATEMENT} p${PRT_COUNT} int )"
+   else
+      STATEMENT="${STATEMENT}"
+   fi
+   STATEMENT="${STATEMENT} CLUSTERED BY (c1) SORTED BY (c1 ASC) INTO 128 BUCKETS;"
+   echo ${STATEMENT}
+}
+genRandomStr() {
+   local len=$1
+   local s=""
+   local chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+   while (( ${#s} < len )); do
+     s+="${chars:RANDOM%62:1}"
+   done
+   echo "$s"
 }
 genData(){
-STATEMENT="INSERT INTO ${TBL_NAME} ("
-((TBL_COLS--))
-#
-# Columns
-#
-for i in $(seq 1 ${TBL_COLS}); do
-STATEMENT="${STATEMENT} c${i},"
-done
-((TBL_COLS++))
-STATEMENT="${STATEMENT} c${TBL_COLS}"
-#
-# Partitions
-#
-if [ "${PRT_COUNT}" -gt 0 ]; then
-STATEMENT="${STATEMENT},"
-((PRT_COUNT--))
-for i in $(seq 1 ${PRT_COUNT}); do
-STATEMENT="${STATEMENT} p${i},"
-done
-((PRT_COUNT++))
-STATEMENT="${STATEMENT} p${PRT_COUNT} )"
-else
-STATEMENT="${STATEMENT} )"
-fi
-#
-# Creating fake dataset per partition
-# Column Data per partition
-# First column is always INT
-# Next are random 15 char strings
-#
-# RowData
-#
-#echo "Line 71: PRT_COUNT=${PRT_COUNT}"
-STATEMENT="${STATEMENT} VALUES ("
-iteration=0
-plist=()
-for r in $(seq 1 ${ROW_PART}); do
-vals="${r},"
-#data=$(tr -dc A-Za-z0-9 </dev/urandom 2>/dev/null| head -c 15)
-data=$(genRandomStr 15)
-((TBL_COLS--))
-for i in $(seq 2 ${TBL_COLS}); do
-vals="${vals} '${data}', "
-done
-((TBL_COLS++))
-vals="${vals} '${data}'"
-STATEMENT="${STATEMENT} ${vals}"
-#
-# PartitionData
-#
-#echo ${STATEMENT}
-if [ "${PRT_COUNT}" -ge 1 ]; then
-STATEMENT="${STATEMENT},"
-#echo "iteration=${iteration}"
-#plist=()
-if [ "${iteration}" -eq 0 ]; then
-#echo "Random partitions: ${PRT_COUNT}"
-for i in $(seq 1 ${PRT_COUNT}); do
-plist=("${plist[@]}" "${RANDOM}")
-done
-((iteration++))
-#echo "End Random partitions"
-fi
-for p in "${plist[@]}"; do
-STATEMENT="${STATEMENT} ${p}, "
-done
-STATEMENT="${STATEMENT::-2}"
-STATEMENT="${STATEMENT} ), ("
-#echo "Line 104"
-else
-STATEMENT="${STATEMENT} )"
-[ "${r}" -lt "${ROW_PART}" ] && STATEMENT="${STATEMENT}, ("
-fi
-((r++))
-done
-STATEMENT=${STATEMENT::-3}
-STATEMENT="${STATEMENT};"
-echo "${STATEMENT}"
+   STATEMENT="INSERT INTO ${TBL_NAME} ("
+   ((TBL_COLS--))
+   #
+   # Columns
+   #
+   for i in $(seq 1 ${TBL_COLS}); do
+       STATEMENT="${STATEMENT} c${i},"
+   done
+   ((TBL_COLS++))
+   STATEMENT="${STATEMENT} c${TBL_COLS}"
+   #
+   # Partitions
+   #
+   if [ "${PRT_COUNT}" -gt 0 ]; then
+      STATEMENT="${STATEMENT},"
+      ((PRT_COUNT--))
+      for i in $(seq 1 ${PRT_COUNT}); do
+          STATEMENT="${STATEMENT} p${i},"
+      done
+      ((PRT_COUNT++))
+      STATEMENT="${STATEMENT} p${PRT_COUNT} )"
+   else
+      STATEMENT="${STATEMENT} )"
+   fi
+   #
+   # Creating fake dataset per partition
+   # Column Data per partition
+   # First column is always INT
+   # Next are random 15 char strings
+   #
+   # RowData
+   #
+   STATEMENT="${STATEMENT} VALUES ("
+   iteration=0
+   plist=()
+   for r in $(seq 1 ${ROW_PART}); do
+       vals="${r},"
+       #data=$(tr -dc A-Za-z0-9 </dev/urandom 2>/dev/null| head -c 15)
+       data=$(genRandomStr 15)
+       ((TBL_COLS--))
+       for i in $(seq 2 ${TBL_COLS}); do
+           vals="${vals} '${data}', "
+       done
+       ((TBL_COLS++))
+       vals="${vals} '${data}'"
+       STATEMENT="${STATEMENT} ${vals}"
+       #
+       # PartitionData
+       #
+       #echo ${STATEMENT}
+       if [ "${PRT_COUNT}" -ge 1 ]; then
+          STATEMENT="${STATEMENT},"
+          #echo "iteration=${iteration}"
+          #plist=()
+          if [ "${iteration}" -eq 0 ]; then
+             #echo "Random partitions: ${PRT_COUNT}"
+             for i in $(seq 1 ${PRT_COUNT}); do
+                 plist=("${plist[@]}" "${RANDOM}")
+             done
+             ((iteration++))
+             #echo "End Random partitions"
+          fi
+          for p in "${plist[@]}"; do
+              STATEMENT="${STATEMENT} ${p}, "
+          done
+          STATEMENT="${STATEMENT::-2}"
+          STATEMENT="${STATEMENT} ), ("
+          #echo "Line 104"
+       else
+          STATEMENT="${STATEMENT} )"
+          [ "${r}" -lt "${ROW_PART}" ] && STATEMENT="${STATEMENT}, ("
+       fi
+       ((r++))
+   done
+   STATEMENT=${STATEMENT::-3}
+   STATEMENT="${STATEMENT};"
+   echo "${STATEMENT}"
 }
-
 main(){
-createTbl
-if [ "${ITERATIONS}" -gt 1 ]; then
-for i in $(seq 1 ${ITERATIONS}); do
-genData
-done
-fi
+   createTbl
+   if [ "${ITERATIONS}" -gt 1 ]; then
+      for i in $(seq 1 ${ITERATIONS}); do
+          genData
+      done
+   fi
 }
-
 main | tee -a /tmp/createTable.${TIMESTAMP}.hql
 beeline -f /tmp/createTable.${TIMESTAMP}.hql
 #EOF
