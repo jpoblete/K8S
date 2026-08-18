dcb3237b17812dc187bd493abddd8871c2c031dd
José Martínez Poblete <j.martinez.poblete@gmail.com>
2026-08-18 12:10:52 -0400

    Update storage format in createTable.sh script
    Using ORC file format for external tables

--- hive/base/createTable.sh
+++ hive/base/createTable.sh
@@ -67,9 +67,9 @@ createTbl(){
    fi   
    STATEMENT="${STATEMENT} CLUSTERED BY (c1) SORTED BY (c1 ASC) INTO 128 BUCKETS"
    if [ "${TBL_TYPE}" == "ice" ]; then
-      STATEMENT="${STATEMENT} STORED BY iceberg;"
+      STATEMENT="${STATEMENT} STORED BY ICEBERG STORED AS ORC;"
    else
-      STATEMENT="${STATEMENT};" 
+      STATEMENT="${STATEMENT} STORED AS ORC;" 
    fi
    echo "${STATEMENT}"
 }
